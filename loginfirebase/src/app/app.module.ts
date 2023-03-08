35cd42442406e47140374c98b370e6f788431c06
Carlos Azcarraga <tasajos@gmail.com>
2023-03-08 17:57:38 -0400

    registro completo

--- loginfirebase/src/app/app.module.ts
+++ loginfirebase/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 //Componentes
 
@@ -33,7 +35,9 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp (environment.firebaseConfig)
+    AngularFireModule.initializeApp (environment.firebaseConfig),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- loginfirebase/src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ loginfirebase/src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { UrlSerializer } from '@angular/router';
+import { Router, UrlSerializer } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -11,9 +12,13 @@ import { UrlSerializer } from '@angular/router';
 export class RegistrarUsuarioComponent implements OnInit{
 
   registrarUsuario: FormGroup;
-
+  loading: boolean = false;
   constructor (private fb: FormBuilder,
-    private afAuth: AngularFireAuth ){
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    
+    ){
 this,this.registrarUsuario = this.fb.group({
   email:['', Validators.required],
   password:['',Validators.required],
@@ -30,11 +35,23 @@ registrarform (){
   const email = this.registrarUsuario.value.email;
   const password = this.registrarUsuario.value.password;
   const repetirpassword = this.registrarUsuario.value.repetirpassword;
-  
+
+  if(password !== repetirpassword){
+
+    alert(('Las contraseñas deben ser las mismas'));
+    return;
+  }
+
+this.loading =   true;
   this.afAuth.createUserWithEmailAndPassword(email,password).then((user) => {
+    this.loading=false;
+    alert(('El usuario fue registrado con exito'));
+    this.router.navigate(['/login']);
 console.log (user);
   }).catch((error) => {
     console.log(error);
+    //this.toastr.error(this.firebaseError(error.code),'Error')
+    this.loading=false;
     alert(this.firebaseError(error.code))
   })
   }
