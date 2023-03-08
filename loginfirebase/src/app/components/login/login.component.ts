127bc942d51b6d77fb9b1f5829ac59c586a665ce
Carlos Azcarraga <tasajos@gmail.com>
2023-03-08 19:18:41 -0400

    login

--- loginfirebase/src/app/components/login/login.component.ts
+++ loginfirebase/src/app/components/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginUsuario : FormGroup;
+  
+  constructor (private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    )
+        {
+          this.loginUsuario = this.fb.group({
+
+            email:['', Validators.required],
+            password:['', Validators.required],
+          })
+    }
+  ngOnInit(): void {
+    
+  }
+
+  loginS (){
+    const email = this.loginUsuario.value.email;
+    const password = this.loginUsuario.value.password;
+
+    this.afAuth.signInWithEmailAndPassword(email,password).then ((user) => {
+
+      console.log(user);
+      this.router.navigate (['/dashboard']);
+    }).catch ((error) => {
+      console.log(error);
+    })
+    
+     
+
+    console.log(email,password)
+  }
+
+  
 }
