61cd171801be27c344ffe4c1dbd42037a3845dff
Carlos Azcarraga <tasajos@gmail.com>
2023-03-08 17:11:28 -0400

    update

--- loginfirebase/src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ loginfirebase/src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -34,9 +34,23 @@ registrarform (){
   this.afAuth.createUserWithEmailAndPassword(email,password).then((user) => {
 console.log (user);
   }).catch((error) => {
-
     console.log(error);
+    alert(this.firebaseError(error.code))
   })
+  }
+
+  firebaseError(code:string){
+switch(code){
+  case 'auth/email-already-in-use':
+    return 'El usuario ya existe'
+  case 'auth/weak-password':
+    return 'La contraseña es muy debil'
+ case 'auth/invalid-email':
+    return 'Correo Invalido'
+  default:
+    return 'Error desconocido'
+}
+
   }
 
 }
